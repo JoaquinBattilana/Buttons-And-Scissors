fe82fc1a43f2a7e7d4154456acbad917d6013e58
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2016-10-29 14:02:00 -0300

    Alguien que lo haga, no se olviden de tabular

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "backEnd.h"
+#include "frontEnd.h" //despues poner el nombre original de los archivos como lo pide la consigna
+
+int main(void)
+{
+    menu();
+
+    //blah blah blah
+    //codigo codigo cogido
+    //paja
+
+
+    return 0;
+}
