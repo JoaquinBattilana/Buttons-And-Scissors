a538cba7a6bb90f9f2ed740582a0d1d71f568777
JoaquinBattilana <jbattilana@itba.edu.ar>
2016-10-30 01:33:07 -0300

    Update tp.c

--- tp.c
+++ tp.c
@@ -4,13 +4,19 @@
 #include "random.h"
 #include <ctype.h>
 
+#define limpiaBuffer while(getchar()!='\n');
 #define MAXMATRICESPORARCHIVO 5
-enum errores {SIN_ERROR=0, ERROR, E_MEM_DIN,E_ARCHIVO_MATRICES,E_ABRIR_ARCHIVO};
+enum errores {SIN_ERROR=0, ERROR, E_MEM_DIN,E_ARCHIVO_MATRICES,E_ABRIR_ARCHIVO, E_NOMBRE_ARCHIVO, E_CREAR_ARCHIVO};
+enum retornos {FALSO=0, VERDADERO};
 typedef enum errores error;
 
 //aclaro que algunas cosas de final de archivo las tube que validar con \n y EOF porque con el editor de texto que tenia
 //parece que automaticamente me agregaba un \n, no se si es por el editor o por linux, pero por las dudas valide las 2
 
+static int escribirArchivo(char * , char ** , int , int , int );
+static int existeArchivo( char * );
+int guardarJuego(char ** , int , int , int );
+static int sobrescribir();
 static int buscarMatriz(FILE*, int, int);
 static int escribirMatriz(FILE*, char **, int);
 static void liberarMatrizCuadrada(char **, int);
@@ -34,8 +40,83 @@ int main(void){
     return SIN_ERROR;
 }
 
+
+int guardarJuego(char ** tablero, int tipo_juego, int turno, int n){
+    int error=SIN_ERROR;
+    int flag=0;
+    char whitespace=0;
+    char nombreArchivo[11];
+    do{
+        printf("Nombre del archivo?: ");
+        scanf("%10s%c", nombreArchivo, &whitespace);
+        if(whitespace=='\n'){
+            if (existeArchivo(nombreArchivo) && sobrescribir()){
+                    error = escribirArchivo(nombreArchivo, tablero,tipo_juego, turno, n);
+                }
+            else {
+                error = escribirArchivo(nombreArchivo, tablero,tipo_juego, turno, n);
+            }
+        }
+        else{
+            error = E_NOMBRE_ARCHIVO;
+            printf(error);
+            limpiaBuffer
+            error=SIN_ERROR;
+        }
+    } while (!error);
+    if(error)
+        printError(error);
+    return error;
+}
+
+
+
+static int escribirArchivo(char * nombreArchivo, char ** tablero, int tipo_juego, int turno, int n){
+    int i,j;
+    FILE * archivo;
+    int error=SIN_ERROR;
+    archivo = fopen(nombreArchivo, "wb");
+    if (archivo!=NULL){
+        fputc(tipo_juego, archivo);
+        fputc(turno, archivo);
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++)
+                fputc(tablero[i][j], archivo);
+        }
+    }
+    else
+        error=E_CREAR_ARCHIVO;
+    if (error)
+        printError(error);
+    fclose(archivo);
+    return error;
+
+}
+
+static int sobrescribir(){
+    char respuesta;
+    do{
+    limpiaBuffer
+    printf("Hay un archivo con el mismo nombre, desea sobrescribirlo?(S/N) \n");
+    scanf("%c", &respuesta);
+    }
+    while( (respuesta!='s' || (respuesta!='n')));
+    return 'n'-respuesta;
+}
+
+static int existeArchivo(char * nombreArchivo){
+    int error = VERDADERO;
+    FILE * aux;
+    aux = fopen(nombreArchivo, "r");
+    if (aux == NULL)
+        error=FALSO;
+    return error;
+}
+
 void printError(int error){
-    char * s_errores[]={"No hay error\n", "Error\n", "Error en la memoria dinamica\n", "Error, el archivo de matrices esta mal hecho\n", "Error, el archivo esta corrupto o no existe\n"};
+    char * s_errores[]={"No hay error\n", "Error\n", "Error en la memoria dinamica\n", "Error, el archivo de matrices esta mal hecho\n",
+                        "Error, el archivo esta corrupto o no existe\n", "Error en el nombre de archivo \n",
+                        "Error al crear/sobrescribir el archivo \n"};
     printf("%s\n", s_errores[error]);
     return;
 }
@@ -43,8 +124,8 @@ void printError(int error){
 
 char ** matrizDsdArchivo(int n){
     error error=SIN_ERROR;
-    int i, c, whitespace=0;
-    char nombreArchivo[10];
+    int i, c;
+    char nombreArchivo[10], whitespace=0;
     sprintf(nombreArchivo, "%dx%d", n,n );
     char pwd[10]="./";
     char ** aux=NULL;
@@ -151,4 +232,3 @@ static void liberarMatrizCuadrada(char ** matriz, int n){
 //
 // Created by joaquin on 29/10/16.
 //
-
