063d3b02b90c4a63ee730d9ebafb93aa59f0740a
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2016-11-01 17:29:13 -0300

    Update buttonsAndScissorsBack.c

--- buttonsAndScissorsBack.c
+++ buttonsAndScissorsBack.c
@@ -30,21 +30,6 @@ typedef struct
 
 char dir_inc[INC_MAX][2] = {{0,1},{1,1},{1,0},{1,-1}}; //incremento direcciones DERECHA, D_ABAJO, ABAJO, I_ABAJO
 
-int calcularMovPcEnDir(matriz_t tablero, punto_t pos, punto_t dir, char boton, int (*cond)(int), size_t dim, movimiento_t * mov_vec);
-void limitesSubMatriz(punto_t p1, punto_t p2, int * minFil, int * maxFil, int * minCol, int * maxCol);
-int esMovimientoValido(matriz_t tablero, punto_t pos, movimiento_t mov, punto_t dir, char boton);
-movimiento_t * sobreescribir(movimiento_t * mov_vec, movimiento_t mov, size_t * dim);
-movimiento_t * agregar(movimiento_t * mov_vec, movimiento_t mov, size_t * dim);
-int realizarCorte(matriz_t * tablero, movimiento_t mov, punto_t dir);
-void calcularDireccion(movimiento_t mov, punto_t * direccion);
-int buscarBoton(matriz_t tablero, punto_t pos, char boton);
-void calcularMovPc(matriz_t tablero, movimiento_t * mov);
-int hayMovimientosValidos(matriz_t tablero);
-int realizarCortePc(matriz_t * tablero);
-void imprimirTablero(matriz_t tablero);
-int condMaxMov(int cantBotones);
-int condMinMov(int cantBotones);
-
 void imprimirTablero(matriz_t tablero)
 {
     printf("\n   ");
