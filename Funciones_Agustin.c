4d1b9749fc1d0bf49a99b6c7ba7e50ba53654f11
AgustinAlejoDammiano <adammiano@itba.edu.ar>
2016-11-02 16:47:39 -0300

    Update Funciones_Agustin.c

--- Funciones_Agustin.c
+++ Funciones_Agustin.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #define LIMPIAR_BUFFER while(getchar()!=EOF)
+#define BLOQUE 10
 
 typedef struct
 {
@@ -42,75 +43,93 @@ int validar(movimiento_t * mov, matriz_t tablero)
 {
     int flag = 0;
     if (mov->origen.y != mov->destino.y || mov->origen.x != mov->destino.x || mov->destino.x != mov->origen.x - mov->origen.y + mov->destino.y)
-    {
-        flag = 1;
-        printf("Los botones no se encuentran en la misma horizontal, vertical o diagonal\n");
-    }
+        flag = 4;
     if (!flag && mov->origen.x > dim && mov->origen.y > dim)
-    {
-        flag = 1;
-        printf("No existe la posición:[%d,%d]\n", mov->inicial[0], mov->inicial[1]);
-    }
+        flag = 5;
     if (!flag && mov->destino.x > dim && mov->destino.y > dim)
-    {
-        flag = 1;
-        printf("No existe la posición:[%d,%d]\n", mov->final[0], mov->final[1]);
-    }
+        flag = 6;
     if (!flag && tablero.v[mov->origen.x][mov->origen.y] != tablero.v[mov->destino.x][mov->destino.y])
-    {
-        flag = 1;
-        printf("Los botones no son del mismo color\n");
-    }
+        flag = 7;
     punto_t direccion;
     calcularDireccion(*mov, &direccion);
     if (!flag && esMovimientoValido(tablero, mov->origen, *mov, direccion))
-    {
-        flag = 1;
-        printf("El corte no tiene una única variedad de botones\n");
-    }
-
+        flag = 8;
     return flag;
 }
 
-void(char * s)
-{
-    for(i=0; c=getchar() !=
+char * vec_error[] = {
+    "",
+    "Comando invalido\n",
+    "Nombre nulo\n",
+    "Nombre demasiado largo\n",
+    "Los botones no se encuentran en la misma horizontal, vertical o diagonal\n",
+    "",
+    "",
+    "Los botones no son del mismo color\n",
+    "El corte no tiene una única variedad de botones\n"
+}
+    
+    
 
 void leer_movimiento(movimiento_t * mov, matriz_t tablero)
 {
-    char estado, ultimo_caracter, flag_error = 0, vector[16], flag_salida = 1, *nombre_archivo;
-    printf("Ingrese el comando:\n");
+    char ultimo_caracter, flag_error = 0, vector[267], nombre_archivo[256];
+    int i, k;
+    printf("Ingrese el comando:\n");5
     do
     {
         flag = 1
         ultimo_caracter = 0;
-        fgets(vector, 16, stdin);
+        fgets(vector, 267, stdin);
         sscanf("[%d,%d] [%d,%d]%c", mov->origen.x, mov->origen.y, mov->destino.x, mov->destino.y, &ultimo_caracter);
         if(ultimo_caracter == '\n')
-            flag_salida= validar(mov, tablero);
-        else if(!strcmp(vector, "savefile "))
+            flag_error = validar(mov, tablero);
+        else if(vector[0] == 's' && vector[1] == 'a' && vector[2] == 'v' && vector[3] == 'e' 
+                && vector[4] == 'f' && vector[5] == 'i' && vector[6] == 'l' && vector[7] == 'e'
+                && vector[8] == ' ')
              {
-                \\guardar nombre dinamicamente
+                for(i = 9, k = 0 ; vector[i] != '\n' && i < 267; i++, k++)
+                    nombre_archivo[k] = vector[i];
+                nombre_archivo[++k] = '\0';
+                if(i == 267)
+                {
+                    flag_error = 3;
+                    LIMPIAR_BUFFER;
+                }
+                else if(k == 1)
+                        flag_error = 2;
+                     else
+                        flag_error = guardarJuego(nombre_archivo, tablero);
              }
         else if(!strcmp(vector, "quit"))
              {
                   printf("¿Está seguro que quiere salir (Y/N)?\n");
-                  fgets(vector, 16, stdin);
+                  fgets(vector, 3, stdin);
                   if(vector[0]=='Y' && vector[1]=='\n')
                   {
                      printf("¿Desea guardar la partida antes de salir (Y/N)?\n");
-                     fgets(vector, 16, stdin);
+                     fgets(vector, 3, stdin);
                      if(vector[0]=='Y' && vector[1]=='\n')
                      {
                         printf("Ingrese el nombre del archivo:\n");
-                        \\guarda nombre dinamicamente
-                        flag_salida = 0;
+                        for(k = 0 ; ultimo_caracter = getchar() != '\n' && i < 256; k++)
+                            nombre_archivo[k] = vector[k];
+                        nombre_archivo[++k] = '\0';
+                        if(i == 256)
+                            flag_error = 3;
+                            LIMPIAR_BUFFER;
+                        else if(k == 1)
+                            flag_error = 2;
+                             else
+                               flag_error = guardarJuego(nombre_archivo, tablero);
                      }
                      else if(vector[0]=='N' && vector[1]=='\n')
-                             flag_salida = 0;
+                             exit(1);
+                          else
+                              flag_error = 1;
                   }
                   else if(vector[0]=='N' && vector[1]=='\n')
-                             flag = 1;
+                             exit(2);
              }
              else
              {
@@ -119,9 +138,13 @@ void leer_movimiento(movimiento_t * mov, matriz_t tablero)
              }
                      
         }
-        if(flag_error)
-            printf("comando invalido\n");
-    }while (flag_salida);
+        if(flag_error == 5)
+            printf("No existe la posición:[%d,%d]\n", mov->origen.x, mov->origen.y);
+        else if(flag_error == 6)
+                 printf("No existe la posición:[%d,%d]\n", mov->destino.x, mov->destino.y);
+             else
+                 printf("Error: %s", vec_error[flag_error]);
+    }while (flag_error);
 }
 
 int menu()
