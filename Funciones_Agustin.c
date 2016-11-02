0db150d710736d705a13bef35b6ad04428fc4916
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2016-11-02 19:15:53 -0300

    Update Funciones_Agustin.c

--- Funciones_Agustin.c
+++ Funciones_Agustin.c
@@ -1,10 +1,10 @@
-
+#include "buttonsAndScissorsBack.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 #define LIMPIAR_BUFFER while(getchar()!=EOF)
 #define BLOQUE 10
-
+/*
 typedef struct
 {
     char ** v;
@@ -23,22 +23,7 @@ typedef struct
     punto_t destino;
     int cantBotones;
 } movimiento_t;
-
-int calcularMovPcEnDir(matriz_t tablero, punto_t pos, punto_t dir, char boton, int (*cond)(int), size_t dim, movimiento_t * mov_vec);
-void limitesSubMatriz(punto_t p1, punto_t p2, int * minFil, int * maxFil, int * minCol, int * maxCol);
-int esMovimientoValido(matriz_t tablero, punto_t pos, movimiento_t mov, punto_t dir, char boton);
-movimiento_t * sobreescribir(movimiento_t * mov_vec, movimiento_t mov, size_t * dim);
-movimiento_t * agregar(movimiento_t * mov_vec, movimiento_t mov, size_t * dim);
-int realizarCorte(matriz_t * tablero, movimiento_t mov, punto_t dir);
-void calcularDireccion(movimiento_t mov, punto_t * direccion);
-int buscarBoton(matriz_t tablero, punto_t pos, char boton);
-void calcularMovPc(matriz_t tablero, movimiento_t * mov);
-int hayMovimientosValidos(matriz_t tablero);
-int realizarCortePc(matriz_t * tablero);
-void imprimirTablero(matriz_t tablero);
-int condMaxMov(int cantBotones);
-int condMinMov(int cantBotones);
-
+*/ //ESTO ESTA SOLO COMO REFERENCIA PARA AGUSTIN
 int validar(movimiento_t * mov, matriz_t tablero)
 {
     int flag = 0;
